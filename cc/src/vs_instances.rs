34ec18576d09c6a0b5c98c20165efadf8084f320
Danny Willems <be.danny.willems@gmail.com>
2025-01-24 17:38:21 +0100

    Enforce cc to 1.0.79

--- cc/src/vs_instances.rs
+++ cc/src/vs_instances.rs
@@ -4,7 +4,7 @@ use std::convert::TryFrom;
 use std::io::BufRead;
 use std::path::PathBuf;
 
-use crate::windows::setup_config::{EnumSetupInstances, SetupInstance};
+use crate::setup_config::{EnumSetupInstances, SetupInstance};
 
 pub enum VsInstance {
     Com(SetupInstance),
@@ -73,7 +73,7 @@ impl TryFrom<&Vec<u8>> for VswhereInstance {
     fn try_from(output: &Vec<u8>) -> Result<Self, Self::Error> {
         let map: HashMap<_, _> = output
             .lines()
-            .map_while(Result::ok)
+            .filter_map(Result::ok)
             .filter_map(|s| {
                 let mut splitn = s.splitn(2, ": ");
                 Some((splitn.next()?.to_owned(), splitn.next()?.to_owned()))
